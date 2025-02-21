8d39c31e1778cd4854cf4fdc5a5e6347de46cd9e
brandon.m.moye@gmail.com <brandon.m.moye@gmail.com>
2025-02-20 18:19:54 -0600

    starting over - basic Node server setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,47 +4,21 @@ import Members from "./components/Members";
 import { use } from "react";
 
 function App() {
-  const [members, setMembers] = useState([]);
-  const [error, setError] = useState(null);
-  const [stateCode, setStateCode] = useState("AL"); //default
-  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState([]);
 
-  const handleFetch = async () => {
-    if (!stateCode) return;
-    const fetchMembers = async () => {
-      try {
-        const response = await fetch(
-          `http://localhost:5001/api/members?state=${stateCode}`
-        ); // making request to node server
-        if (!response.ok) {
-          throw new Error(`http Error: ${response.status}`);
-        }
-        const data = await response.json();
-        setMembers(data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchMembers();
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:5001/api/test");
+      const data = await response.json();
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
   };
 
   return (
     <>
       just a bill
-      <input
-        type="text"
-        value={stateCode}
-        onChange={(e) => setStateCode(e.target.value.toUpperCase())}
-        maxLength={2}
-      ></input>
-      <button onClick={handleFetch} disabled={loading}>
-        Submit
-      </button>
-      {members.map((member, index) => (
-        <Members key={index} member={member} />
-      ))}
+      <button onClick={fetchData}>Fetch Data</button>
     </>
   );
 }
